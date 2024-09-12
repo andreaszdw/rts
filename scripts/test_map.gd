8c575bba61e672d5c7b108037be6c1bfed9a34d6
andreaszdw <andreaszdw@googlemail.com>
2024-09-12 15:32:19 +0200

    update navigation

---
+++ scripts/test_map.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var new_region_rid: RID = NavigationServer2D.region_create()
+	var default_map_rid: RID = get_world_2d().get_navigation_map()
+	NavigationServer2D.region_set_map(new_region_rid, default_map_rid)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			print("pressed left at ", event.position)
+			$TankProto.set_movement_target(event.position)
